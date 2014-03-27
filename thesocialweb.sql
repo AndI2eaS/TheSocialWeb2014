82a6bd4dfaddfbc65c6e7975516ac621cd9f5a34
Karadimas Andreas <andreas.krdms@gmail.com>
2014-03-27 11:33:08 +0100

    Create thesocialweb.sql

---
+++ thesocialweb.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+-- PHP: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- DATABASE: `thesocialweb`
+--
+CREATE DATABASE IF NOT EXISTS `thesocialweb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `thesocialweb`;
+
+-- --------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tweet` varchar(200) NOT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `opinion` varchar(10) NOT NULL,
+  `label` char(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
