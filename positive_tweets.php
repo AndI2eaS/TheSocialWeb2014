e35c2dc42d1c98e6d4e4c1c3cfb8c77c8d4216cd
Karadimas Andreas <andreas.krdms@gmail.com>
2014-03-27 11:30:10 +0100

    Create positive_tweets.php

---
+++ positive_tweets.php
@@ -0,0 +1,40 @@
+<?php include "top.php"; ?>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Group 11 - Carsideror</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+	</head>
+	<body>
+		<?php
+			include "header.php";
+		?>		
+		<div class="new_main">
+			<?php
+			$post = mysql_real_escape_string($_GET['key_select']);
+			echo '<a href=first.php?key_select=' . $post . '>Back</a><br /><br />';
+			
+			// Select the classified as positive tweets from the database for a specific chosen brand
+			$query = "SELECT tweet, opinion, label FROM results WHERE label='$post' AND opinion='Positive' ORDER BY id DESC LIMIT 80";
+			$result = mysql_query($query);
+			echo "<table>";
+			echo "<th>Tweets</th>";
+			echo "<th>Opinion</th>";
+			while ($row = mysql_fetch_array($result)){
+				// Show the data (tweets) to the user
+				echo "<tr>";
+				echo "<td>" . $row['tweet'] . "</td>";
+				echo "<td>" . $row['opinion'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+			?>
+		</div>
+		<br />
+		<br />
+		<hr />
+		<?php 
+			include "footer.php";
+		?>
+	</body>
+</html>
